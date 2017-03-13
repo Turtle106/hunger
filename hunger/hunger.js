9df96b91565fcde16154cb2833d23246240bfaa8
Turtle106 <adawse11ers@gmail.com>
2017-03-13 07:55:05 -0600

    Changelog

--- hunger/hunger.js
+++ hunger/hunger.js
@@ -41,7 +41,7 @@ function story() {
         while (randTribute === randTribute3 || randTribute3 === randTribute2) {
             var randTribute3 = tributesLiving[Math.floor(Math.random()*tributesLiving.length)];
         }
-        var immediateEventsDeath = [" is shot by " + randTribute2 +  " with a bow", " is blown skyhigh by the capital", " is stabbed with a sword", " is impaled by a thrown dagger", " is strangled by " + randTribute2, " is killed by a wild charmander", " is viciously decapitated by " + randTribute2];
+        var immediateEventsDeath = [" is shot by " + randTribute2 +  " with a bow", " is blown skyhigh by the capital", " is stabbed with a sword", " is impaled by a thrown dagger", " is strangled by " + randTribute2, " is killed by a snare set by " + randTribute2, " is viciously decapitated by " + randTribute2];
     
         var immediateEvents = [" is nearly killed by " + randTribute2, " finds a stream and collects water", " grabs a backpack and runs", " runs from the center", " evades the bloodbath", " grabs a dagger and runs", " teams with " + randTribute2 + " and " + randTribute3, " hides near the action watching", " runs screaming", " narrowly escapes with a bow and a couple dozen arrows"]
         var randomNumberBetween0and3 = Math.floor(Math.random() * 4);
@@ -55,10 +55,7 @@ function story() {
         }
     }
 
-    
-        
-    console.log("Log1: ", tributesLiving)
-    while (tributesLiving.length > 1) {console.log("I made it!")
+    while (tributesLiving.length > 1) {
          v = Math.floor(Math.random()*tributesLiving.length)
          randTribute = tributesLiving[v];
          randTribute2 = tributesLiving[Math.floor(Math.random()*tributesLiving.length)];
@@ -66,7 +63,7 @@ function story() {
             var randTribute2 = tributesLiving[Math.floor(Math.random()*tributesLiving.length)];
         }
     
-    var mortalReminders = [" falls off a cliff", " eats poisonous berries", " is shot by " + randTribute2 +  " with a bow", " burns in a fire", " dies in an explosion", " is trampled by genetically altered rabid squirrels", " eats poisonous mushrooms", " unknowingly walks into a giant venus fly trap thinking it's a cave", " dies of sleep deprevation", " dies of thirst", " dies of infection", " walks into a high voltage forcefield"];
+    var mortalReminders = [" falls off a cliff", " eats poisonous berries and dies", " is shot by " + randTribute2 +  " with a bow", " burns in a fire", " dies in an explosion", " is trampled by genetically altered rabid squirrels", " eats poisonous mushrooms", " unknowingly walks into a giant venus fly trap thinking it's a cave", " dies of sleep deprevation", " dies of thirst", " dies of infection", " walks into a high voltage forcefield"];
     var events = [" is sent medical supplies from an unknown sponsor", " is nearly killed by " + randTribute2, " finds a stream and collects water", " spies on " + randTribute2, " hides", " steals food from " + randTribute2, " befriends an army of squirrels", " catches a wild geodude", " finds a mech suit", " questions life", " eats pie", " resorts to cannibalism", " talks to a butterfly", " sleeps in a tree", " gets on the bad side of the capital", " starts hallucinating", " wonders what comes after life", " makes a makeshift spear", " treats wounds", " recieves a hatches from an unknown sponsor", " says: " + randTribute2 + ", there's a forcefield back there"];
         var randomNumberBetween0and6 = Math.floor(Math.random() * 7);
         var item = events[Math.floor(Math.random()*events.length)];
